d41bccc81eec0db2b4868c15d9995d1b08bda21e
Luan Carlo Santos de Paulo <luancarlo.paulo@gmail.com>
2019-11-09 11:10:23 -0300

    master
    Adicionada Função

--- src/calculadora.php
+++ src/calculadora.php
@@ -1,33 +1,40 @@
 <?php
-
-
     $request = json_decode(file_get_contents("php://input"));
 
     $a = floatval ($request->valor_a);
     $b = floatval ($request->valor_b);
     $c = floatval ($request->valor_c);
 
-    try {
+    function calcular ($a, $b,$c)
+    {
+
+        try {
 
 
-        $delta = ($b * $b) - ((4 * $a) * $c);
+            $delta = ($b * $b) - ((4 * $a) * $c);
 
-        $x1 = (-$b + sqrt($delta)) / (2 * $a);
+            $x1 = (-$b + sqrt($delta)) / (2 * $a);
 
-        $x2 = (-$b - sqrt($delta)) / (2 * $a);
+            $x2 = (-$b - sqrt($delta)) / (2 * $a);
 
-        $results = [
-            "success" => true,
-            "x1" => $x1,
-            "x2" => $x2,
-            "delta" => $delta
-        ];
-        $results = json_encode($results);
+            $results = [
+                "success" => true,
+                "x1" => $x1,
+                "x2" => $x2,
+                "delta" => $delta
+            ];
+            $results = json_encode($results);
 
-    } catch(Exception $e) {
-        echo 'Message: ' .$e->getMessage();
+        } catch(Exception $e) {
+            $results = $e->getMessage();
+            $results = json_encode($results);
+        }
+
+        return $results;
     }
 
-    echo $results;
-    return $results;
+    $resultado = calcular($a,$b,$c);
+    echo $resultado;
+
+
 ?>
